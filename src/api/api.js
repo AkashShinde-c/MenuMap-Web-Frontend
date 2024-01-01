a915793712907061a6f08bcd895bad3732b15c13
AkashShinde-c <shindeakashak47@gmail.com>
2024-01-01 19:56:19 +0530

    api url configuration

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://menumap.onrender.com',  
-  baseURL: 'http://13.232.188.31:3000',  
+  baseURL: 'https://menumap-backend-production.up.railway.app',  
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
