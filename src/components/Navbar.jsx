ec59904c38612ab9b3fe0806677714142a22570b
AkashShinde-c <shindeakashak47@gmail.com>
2024-01-12 13:28:41 +0530

    Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useRef, useState } from "react";
 import "../css/Navbar.css";
 import logo from "../assets/logo.svg";
-import logo_round from "../assets/logo_round.svg";
+import logo_round from "../assets/logo.svg";
 import home from "../assets/home.svg";
 import contact from "../assets/contact.svg";
 import about from "../assets/about.png";
 import About from "./About";
 import MyContext from "../context/MyContext";
+import notes from "../assets/notes.png"
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -18,7 +19,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar-custom">
+    <nav className="navbar-custom" id="navbar-custom">
       {showAbout && <About />}
       <div className="left-nav">
         <div className="logo">
@@ -36,8 +37,8 @@ const Navbar = () => {
           <svg
             className="swap-off fill-current"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
+            width="28"
+            height="28"
             viewBox="0 0 512 512"
           >
             <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
@@ -47,14 +48,14 @@ const Navbar = () => {
           <svg
             className="swap-on fill-current"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
+            width="28"
+            height="28"
             viewBox="0 0 512 512"
           >
             <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
           </svg>
         </label>
-        <div className={`menu-show ${showMenu ? "" : "menu-hide"}`} onClick={showMenu ?()=>{toggleMenu(); chceckboxRef.current.checked = false}:()=>{}}>
+        <div className={`menu-show ${showMenu ? "" : "menu-hide"}`} onClick={showMenu ?()=>{toggleMenu(); chceckboxRef.current.checked = false}:()=>{}} anchor="navbar-custom">
         {showMenu && <div className="wrapper   absolute bottom-0 top-0 w-screen h-screen  bg-transparent" onClick={toggleMenu}></div>}
           <div className="nav-item" onClick={toggleMenu}>
             <img src={home} alt="home" />
@@ -76,6 +77,13 @@ const Navbar = () => {
               <div className="menu-item">Contact</div>
             </a>
           </div>
+          <div className="nav-item">
+            <img src={notes} alt="contact" />
+            <a href="https://wa.me/+919373801864" target="_blank">
+              <div className="menu-item">Messages</div>
+            </a>
+          </div>
+
         </div>
       </div>
     </nav>
--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -181,17 +181,7 @@ const MapView = () => {
           </div>
         </InfoWindow>
       )}
-      <div className="btm-nav">
-  <button className="bg-secondary text-base-100">
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-    <span className="btm-nav-label">Map View</span>
-  </button>
-  <button className="active bg-secondary text-base-100 border-blue-600">
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-    <span className="btm-nav-label">Tab View</span>
-  </button>
-  
-</div>
+      
     </GoogleMap>
   ) : (
     <Loader></Loader>
